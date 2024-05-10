15ce34bfc4117062abf2fe3cca21d52ef7c6872d
Peter Gerasimov <gerasimov@mediahub.eu>
2024-05-10 21:34:28 +0300

    added ear cutting

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,5 +1,6 @@
 import { Scene, Math as PhaserMath } from 'phaser';
 import generatePolygon from '../math/generatePolygon';
+import earcut from 'earcut';
 
 export class Game extends Scene {
   constructor() {
@@ -10,16 +11,32 @@ export class Game extends Scene {
 
   create(): void {
 
-    const points = generatePolygon([0,0],
+    const points = generatePolygon(
+      [0,0],
       [Math.PI * 1.2, 1.7 * Math.PI],
       [0.4, 0.7],
       PhaserMath.Between(200, 300), 
       PhaserMath.FloatBetween(0.1, 0.3), 
       PhaserMath.FloatBetween(0.1, 0.2), 
-      PhaserMath.Between(10, 20)
+      5
     );
 
+    const pointsFlat = points.flatMap(p => p);
+    const triangleIndices: number[] = earcut(pointsFlat);
+
+    const colorStep = 255 / (triangleIndices.length * 3) ;
+
     this.add.polygon(this.cameras.main.centerX, this.cameras.main.centerY, points, 0xff0000);
+    for(let i = 0; i < triangleIndices.length; i += 3) {
+      const triangle = [
+        points[triangleIndices[i]],
+        points[triangleIndices[i + 1]],
+        points[triangleIndices[i + 2]]
+      ];
+
+      this.add.polygon(this.cameras.main.centerX, this.cameras.main.centerY, triangle, 0x000000 + i * colorStep);
+    }
+
 
     this.add.text(this.cameras.main.centerX, this.cameras.main.centerY,"GJAMES", {fontSize: 50, backgroundColor: "000000"}).setOrigin(0.5);
 
