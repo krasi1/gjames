e871b88ce2c77e9ae0ce60ea5866cf3bfb63e489
Peter Gerasimov <gerasimov@mediahub.eu>
2024-05-12 14:13:57 +0300

    added game instructions to main menu

--- src/scenes/menu.ts
+++ src/scenes/menu.ts
@@ -28,12 +28,16 @@ export default class Menu extends Scene {
         this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, "Press SPACE to play",
             { fontSize: 50, wordWrap: { width: 600, useAdvancedWrap: true }, align: "center" }).setOrigin(0.5);
 
+        // add text for instructions how to play
+        this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 100, "Use arrow keys to move and space to shoot",
+            { fontSize: 30, wordWrap: { width: 600, useAdvancedWrap: true }, align: "center" }).setOrigin(0.5);
+        this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 300, "Green gems increase fire rate, red gems increase damage, blue gems upgrade weapon, and yellow gems split shot. Avoid the asteroids!",
+            { fontSize: 30, wordWrap: { width: 600, useAdvancedWrap: true }, align: "center" }).setOrigin(0.5);
+
         this.input.keyboard.on("keydown-SPACE", () => this.scene.start('GameScene'))
     }
 
     update(): void {
         this.background.tilePositionY -= config.background.scrollVelocity;
     }
-
-
 }
